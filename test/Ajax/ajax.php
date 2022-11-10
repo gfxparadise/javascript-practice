959f6d651e76a6ac20fbe1484562177611fb7a3c
gfxparadise <abdul.qadir.avadia@gmail.com>
2022-11-10 17:47:11 +0500

    ..

--- test/Ajax/ajax.php
+++ test/Ajax/ajax.php
@@ -1,5 +1,17 @@
-Use to load data without refreashing data 
-<php
-$.ajax({})
+<Script>
+// Use to load data without refreashing data 
+$(document).ready(functiom){
+    $("#load-button").on("click",function(e){
+       $.ajax({
+            url : "ajax-load.php" ,
+            type : "POST" ,
+            success : function(data){
+                $("#tabel-data").html(data);
+            }
+}) 
+    })
+}
 
-?>
+// Way to insert data using ajax without refreashing data
+
+</Script>
